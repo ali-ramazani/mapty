6730de41e287ba225d42711abdf434d0cdd6dccf
M-Ali-Ramazani <112561860+ramazanima@users.noreply.github.com>
2024-01-05 16:26:51 -0500

    completed UI

---
+++ script.js
@@ -0,0 +1,38 @@
+'use strict';
+
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
+function successCallback(position) {
+  const { latitude } = position.coords;
+  const { longitude } = position.coords;
+  const coords = [latitude, longitude];
+  var map = L.map('map').setView(coords, 12);
+
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+
+  L.marker(coords)
+    .addTo(map)
+    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+    .openPopup();
+
+  console.log(position);
+  console.log(latitude, longitude);
+}
+
+function failureCallback() {
+  console.log(`Location not found`);
+}
+
+navigator.geolocation.getCurrentPosition(successCallback, failureCallback);
